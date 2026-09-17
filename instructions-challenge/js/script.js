5c1e72ed3ed0ddc4ec4ff90070940444d550ef7f
CJ <134082536+Sishooboy@users.noreply.github.com>
2026-09-17 16:33:44 -0400

    added to my p5 landscape the main setting meaning. Some grass a blue sky and a sun
    (also changed the project name)

---
+++ instructions-challenge/js/script.js
@@ -0,0 +1,48 @@
+
+"use strict";
+
+function setup() {
+    createCanvas(400, 400);
+
+
+}
+
+function draw() {
+    background(173, 216, 230);
+    noStroke();
+    drawlandscape();
+    Sun();
+    tree();
+}
+
+
+function drawlandscape(){
+    push();
+    fill(240,130,20);
+    square(200,350,400);
+    pop();
+}
+
+function Sun(){
+    push();
+    fill(255, 255, 0);
+    ellipse(300, 100, 50, 50);
+    pop();
+}
+
+function tree(){
+    push();
+    fill(0, 200, 0);
+    circle(300, 300, 30);
+    pop();
+
+    push();
+    fill(0, 200, 0);
+    circle(330, 280, 40);
+    pop();
+
+    push();
+    fill(0, 190, 0);
+    circle(300, 250, 60);
+    pop();
+}
