7dee6cb0b6713c35438f9b793eb050780ce7377c
CJ <charbeljdagher@gmail.com>
2026-09-24 22:26:12 -0400

    Basically done with the last prototype
    Didn't know where I was going with this but I ended up with a red carpet leading to a red demonic pyramid where they do rituals.(it is lighter in color at the bottom of the pyramid because it seems we are in hell or something)

--- instructions-prototypes/abstractsquares/sketch.js
+++ instructions-prototypes/abstractsquares/sketch.js
@@ -8,3 +8,127 @@ function draw() {
     background(128);
     squares();
 }
+function squares() {
+
+    push();
+    fill(0, 0, 100);
+    noStroke();
+    rectMode(CENTER);
+    rect(200, 300, 240, 400);
+    pop();
+
+    push();
+    fill(200, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    rect(200, 300, 200, 400);
+    pop();
+
+    push();
+    fill(200, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 240);
+    pop();
+
+    push();
+    fill(190, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 220);
+    pop();
+
+    push();
+    fill(180, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 200);
+    pop();
+
+    push();
+    fill(170, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 180);
+    pop();
+
+    push();
+    fill(160, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 160);
+    pop();
+
+    push();
+    fill(140, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 140);
+    pop();
+
+    push();
+    fill(120, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 120);
+    pop();
+
+
+    push();
+    fill(80, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 90);
+    pop();
+
+    push();
+    fill(60, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 80);
+    pop();
+
+    push();
+    fill(40, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 70);
+    pop();
+
+    push();
+    fill(20, 0, 0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 60);
+    pop();
+
+    push();
+    fill(0);
+    noStroke();
+    rectMode(CENTER);
+    square(200, 0, 50);
+    pop();
+
+    push();
+    fill(90, 0, 0);
+    stroke(0, 0, 100);
+    strokeWeight(3);
+    rectMode(CENTER);
+    square(200, 150, 50);
+    pop();
+    push();
+    fill(90, 0, 0);
+    stroke(0, 0, 100);
+    strokeWeight(3);
+    rectMode(CENTER);
+    square(200, 250, 50);
+    pop();
+
+    push();
+    fill(90, 0, 0);
+    stroke(0, 0, 100);
+    strokeWeight(3);
+    rectMode(CENTER);
+    square(200, 350, 50);
+    pop();
+}
